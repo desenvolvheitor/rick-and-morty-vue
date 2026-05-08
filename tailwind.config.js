133691dff827831f15cbf38b273172500400fa06
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-08 11:10:02 -0300

    Configuração de variáveis para estilos e importação de fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     './src/**/*.{vue,js,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+      },
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        tertiary: 'var(--tertiary-color)',
+        quaternary: 'var(--quaternary-color)',
+        alive: 'var(--alive-color)',
+        dead: 'var(--dead-color)',
+        unknown: 'var(--unknown-color)',
+        'main-text': 'var(--text-color)',
+        'sec-text': 'var(--sec-text-color)',
+      },
+      borderRadius: {
+        card: 'var(--card-radius)',
+      }
+    },
   },
   plugins: [],
 };
