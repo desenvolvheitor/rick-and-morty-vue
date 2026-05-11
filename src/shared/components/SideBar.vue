c1729d878d7ee4a74e2bcd6b466f325c6f6d6b62
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-11 15:02:34 -0300

    Melhorias e adição da parte funcional da sidebar para filtros de busca

--- src/shared/components/SideBar.vue
+++ src/shared/components/SideBar.vue
@@ -1,23 +1,52 @@
 <script setup lang="ts">
+import { watch } from 'vue';
+import { usePersonagemStore } from '@/shared/stores/personagemStore';
+
+const personagemStore = usePersonagemStore();
+
+const limparFiltros = (): void => {
+  personagemStore.filtros.name = '';
+  personagemStore.filtros.status = '';
+  personagemStore.filtros.species = '';
+  personagemStore.filtros.gender = '';
+};
+
+let timer: ReturnType<typeof setTimeout> | undefined;
+watch(() => ({ ...personagemStore.filtros }), (novo, antigo) => {
+  if (novo.page === antigo.page && personagemStore.filtros.page !== 1) {
+    personagemStore.filtros.page = 1;
+    return;
+  }
+
+  clearTimeout(timer);
+
+  if (novo.name !== antigo.name) {
+      timer = setTimeout(personagemStore.buscarPersonagens, 500);
+  }
+  else {
+    personagemStore.buscarPersonagens();
+  }
+});
 </script>
 
 <template>
-  <aside class="bg-primary border-r border-tertiary flex flex-col gap-12 transition-transform duration-200 fixed p-7 lg:relative z-50 h-screen lg:h-auto w-4/5 lg:w-full">
+  <aside
+    class="bg-primary border-r border-tertiary flex flex-col gap-12 transition-transform duration-200 fixed p-7 lg:relative z-50 h-screen lg:h-auto w-4/5 lg:w-full">
     <form @submit.prevent>
       <div class="flex flex-col">
         <label for="search" class="label-filtro">Busca</label>
-        <input id="search" type="text" name="search" class="campo-filtro cursor-auto" placeholder="Buscar por nome...">
+        <input id="search" v-model="personagemStore.filtros.name" type="text" name="search" class="campo-filtro cursor-auto" placeholder="Buscar por nome...">
       </div>
 
       <div class="flex flex-col">
-        <label for="status" class="label-filtro">Filtros</label>
-        <select id="status" name="status" class="campo-filtro">
+        <label for="status" class="label-filtro mt-4">Filtros</label>
+        <select id="status" v-model="personagemStore.filtros.status" name="status" class="campo-filtro">
           <option value="">Todos os status</option>
           <option value="alive">Alive</option>
           <option value="dead">Dead</option>
           <option value="unknown">Unknown</option>
         </select>
-        <select id="especies" name="species" class="campo-filtro">
+        <select id="especies" v-model="personagemStore.filtros.species" name="species" class="campo-filtro">
           <option value="">Todas as espécies</option>
           <option value="alien">Alien</option>
           <option value="animal">Animal</option>
@@ -31,14 +60,14 @@
           <option value="unknown">Unknown</option>
         </select>
 
-        <select id="generos" name="gender" class="campo-filtro">
+        <select id="generos" v-model="personagemStore.filtros.gender" name="gender" class="campo-filtro">
           <option value="">Todos os gêneros</option>
           <option value="female">Female</option>
           <option value="genderless">Genderless</option>
           <option value="male">Male</option>
           <option value="unknown">Unknown</option>
         </select>
-        <button id="botao-limpar-filtros" type="button" class="bg-secondary text-sec-text rounded-md border border-tertiary transition-all duration-300 ease-in-out hover:border-dead hover:text-dead">Limpar filtros</button>
+        <button id="botao-limpar-filtros" type="button" class="bg-secondary text-sec-text rounded-md p-2 mt-4 border border-tertiary transition-all duration-300 ease-in-out hover:border-dead hover:text-dead" @click="limparFiltros">Limpar filtros</button>
       </div>
     </form>
   </aside>
