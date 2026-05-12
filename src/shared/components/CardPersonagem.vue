7d94ffaa33c12b6cb827413390b4d277c627fc72
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-12 15:40:09 -0300

    Incremento na estilização do CardPersonagem (transição e hover)

--- src/shared/components/CardPersonagem.vue
+++ src/shared/components/CardPersonagem.vue
@@ -12,10 +12,10 @@ const statusInfo = computed(() => {
 </script>
 
 <template>
-  <li class="rounded-2xl overflow-hidden bg-primary flex flex-col">
+  <li class="rounded-2xl overflow-hidden bg-primary flex flex-col w-full transition-all duration-200 ease-in-out group hover:cursor-pointer hover:border hover:border-alive hover:scale-105">
     <img :src="props.personagem.image" :alt="props.personagem.name">
     <div class="py-2 px-3">
-      <h3 class="font-orbitron text-main-text text-xl"><strong>{{ props.personagem.name }}</strong></h3>
+      <h3 class="font-orbitron text-main-text text-xl group-hover:text-alive"><strong>{{ props.personagem.name }}</strong></h3>
       <p class="text-main-text">{{ props.personagem.species }}</p>
       <p :class="statusInfo.color">{{statusInfo.emoji}} {{ props.personagem.status }}</p>
     </div>
