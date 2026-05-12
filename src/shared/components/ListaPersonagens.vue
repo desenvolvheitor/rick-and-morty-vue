81c226081d1938d6c6afacab0d1af555bb64f8f6
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-12 15:41:45 -0300

    Adição da paginação abaixo da lista de personagens

--- src/shared/components/ListaPersonagens.vue
+++ src/shared/components/ListaPersonagens.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { usePersonagemStore } from '@/shared/stores/personagemStore';
 import CardPersonagem from './CardPersonagem.vue';
+import Paginacao from './Paginacao.vue';
 const personagemStore = usePersonagemStore();
 </script>
 
 <template>
-  <ul class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-    <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem"/>
+  <ul class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+    <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem" />
   </ul>
+  <Paginacao />
 </template>
 
