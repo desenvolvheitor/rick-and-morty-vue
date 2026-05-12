7056e1f0ecf021a374a8d5d6b4719a13d7fd03d8
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-12 12:16:55 -0300

    Criação do elemento que agrupará os cards de personagens em uma grade

---
+++ src/shared/components/ListaPersonagens.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { usePersonagemStore } from '@/shared/stores/personagemStore';
+import CardPersonagem from './CardPersonagem.vue';
+const personagemStore = usePersonagemStore();
+</script>
+
+<template>
+  <ul class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem"/>
+  </ul>
+</template>
+
