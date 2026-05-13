e622bee70a07b346f4e6133cd6427d20dfa12033
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-13 11:05:14 -0300

    Estilização da paginação e inclusão de validações booleanas para mostrar ou não, e para deixar desabilitado ou não

--- src/shared/components/Paginacao.vue
+++ src/shared/components/Paginacao.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { usePersonagemStore } from '../stores/personagemStore';
 
 const personagemStore = usePersonagemStore();
+
+const mostrarPaginacao = computed(() => {
+  const temPersonagens = personagemStore.listaPersonagens.length > 0;
+  const temMaisDeUmaPagina = (personagemStore.informacoesPaginacao?.pages ?? 0) > 1;
+
+  return !personagemStore.carregando && temPersonagens && temMaisDeUmaPagina;
+});
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-1 mt-4">
+  <div v-if="mostrarPaginacao" class="flex flex-col items-center justify-center gap-1 mt-4">
     <p class="text-main-text">Página {{personagemStore.filtros.page}} de {{ personagemStore.informacoesPaginacao?.pages }}</p>
     <div class="flex gap-4">
-      <button class="text-alive bg-transparent border border-alive rounded-lg px-4 py-1 text-shadow-glow" @click="personagemStore.paginaAnterior">Página Anterior</button>
-      <button class="text-alive bg-transparent border border-alive rounded-lg px-4 py-1 text-shadow-glow" @click="personagemStore.proximaPagina">Próxima página</button>
+      <button :class="['px-4 py-1 rounded-lg border transition-all text-alive border-alive text-shadow-glow shadow-[0_0_6px] shadow-alive hover:scale-105', personagemStore.paginaAnteriorDisponivel ? '' : 'opacity-50 cursor-not-allowed hover:none']" @click="personagemStore.paginaAnterior"><span>⭠ </span>Página Anterior</button>
+      <button :class="['px-4 py-1 rounded-lg border transition-all text-alive border-alive text-shadow-glow shadow-[0_0_6px] shadow-alive hover:scale-105', personagemStore.proximaPaginaDisponivel ? '' : 'opacity-50 cursor-not-allowed hover:none']" :disabled="!personagemStore.proximaPaginaDisponivel" @click="personagemStore.proximaPagina">Próxima página <span> ⭢</span></button>
     </div>
   </div>
 </template>
