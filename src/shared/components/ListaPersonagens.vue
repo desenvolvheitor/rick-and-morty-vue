081f8f078a279d998357c512371f5e9e70a9ca75
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 12:08:10 -0300

    Corrigindo ordenação indesejada

--- src/shared/components/ListaPersonagens.vue
+++ src/shared/components/ListaPersonagens.vue
@@ -3,13 +3,13 @@ import { usePersonagemStore } from '@/shared/stores/personagemStore';
 import CardPersonagem from './CardPersonagem.vue';
 import Paginacao from './Paginacao.vue';
 import Modal from './Modal.vue';
-import Carregando from './Carregamento.vue';
+import Carregamento from './Carregamento.vue';
 const personagemStore = usePersonagemStore();
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center h-full">
-    <Carregando v-if="personagemStore.carregando" />
+  <div class="flex flex-col items-center h-full">
+    <Carregamento v-if="personagemStore.carregando" />
     <ul v-else class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
       <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem" @click="personagemStore.selecionarPersonagem(personagem), personagemStore.exibirModal" />
     </ul>
