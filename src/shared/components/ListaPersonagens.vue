40cd43d366b42a7a944347bb8c0f417c69333d98
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 15:58:55 -0300

    Criação dos elementos de Feedback do status (erro, carregando ou vazio)

--- src/shared/components/ListaPersonagens.vue
+++ src/shared/components/ListaPersonagens.vue
@@ -3,13 +3,16 @@ import { usePersonagemStore } from '@/shared/stores/personagemStore';
 import CardPersonagem from './CardPersonagem.vue';
 import Paginacao from './Paginacao.vue';
 import Modal from './Modal.vue';
-import Carregamento from './Carregamento.vue';
+import FeedbackStatus from './FeedbackStatus.vue';
+
 const personagemStore = usePersonagemStore();
 </script>
 
 <template>
   <div class="flex flex-col items-center min-h-full pb-1">
-    <Carregamento v-if="personagemStore.carregando" />
+    <FeedbackStatus v-if="personagemStore.listaPersonagens.length < 1 && !personagemStore.carregando" tipo="vazio" />
+    <!-- <FeedbackStatus v-if="" tipo="erro" /> -->
+    <FeedbackStatus v-if="personagemStore.carregando" tipo="carregando" />
     <ul v-else class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
       <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem" @click="personagemStore.selecionarPersonagem(personagem), personagemStore.exibirModal" />
     </ul>
