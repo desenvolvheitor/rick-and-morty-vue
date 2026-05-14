1653167599b2855727eb63bf7ab0b83df0e5e6ea
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 11:55:52 -0300

    Criação de classe para estilização repetitiva e correção no funcionamento do modal (+ estilização)

--- src/shared/components/Modal.vue
+++ src/shared/components/Modal.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { usePersonagemStore } from '../stores/personagemStore';
+
+const personagemStore = usePersonagemStore();
 
 const props = defineProps<{
   personagem: Personagem
 }>();
 
-const modalRef = ref(null);
+const modalRef = ref<HTMLDialogElement | null>(null);
+onMounted(() => {
+  modalRef.value?.showModal();
+});
 </script>
 
 <template>
-  <dialog ref="modalRef" class="absolute rounded-xl top-2/4 left-2/4 max-h-[95vh] w-[25vw] text-main-text border-none outline-none text-wrap -translate-x-1/2 -translate-y-1/2 backdrop:bg-black/60 backdrop:backdrop-blur-sm">
-    <img :src="props.personagem.image" :alt="props.personagem.name"/>
-    <div id="informacoes-modal-personagem">
-      <h2 class="text-main-text">{{props.personagem.name}}</h2>
-      <p class="text-main-text">Status: {{props.personagem.status}}</p>
-      <p class="text-main-text">Espécie: {{props.personagem.species}}</p>
-      <p class="text-main-text">Gênero: {{props.personagem.gender}}</p>
-      <p class="text-main-text">Origem: {{props.personagem.origin.name}}</p>
-      <p class="text-main-text">Localização: {{props.personagem.location.name}}</p>
-      <p class="text-main-text">Episódios: {{props.personagem.episode.length}}</p>
+  <dialog ref="modalRef" class="fixed rounded-xl max-h-[95vh] w-[25vw] text-main-text border-none outline-none text-wrap bg-primary backdrop:bg-black/80 backdrop:backdrop-blur-sm">
+    <img class="w-full" :src="props.personagem.image" :alt="props.personagem.name"/>
+    <div id="informacoes-modal-personagem" class="px-5 py-3 border border-b-px border-tertiary">
+      <h2 class="font-orbitron text-alive text-2xl text-shadow-glow shadow-alive"><strong>{{props.personagem.name}}</strong></h2>
+      <p class="atributo-personagem">Status: {{props.personagem.status}}</p>
+      <p class="atributo-personagem">Espécie: {{props.personagem.species}}</p>
+      <p class="atributo-personagem">Gênero: {{props.personagem.gender}}</p>
+      <p class="atributo-personagem">Origem: {{props.personagem.origin.name}}</p>
+      <p class="atributo-personagem">Localização: {{props.personagem.location.name}}</p>
+      <p class="atributo-personagem">Episódios: {{props.personagem.episode.length}}</p>
     </div>
-    <div id="botao-fechar-modal" aria-text="Fechar">&#x2715;</div>
+    <div id="botao-fechar-modal" class="absolute cursor-pointer top-1 right-1 text-xl w-8 h-8 flex items-center justify-center text-main-text bg-black/50 rounded-full hover:bg-dead hover:text-white transition-all active:scale-90" aria-text="Fechar" @click="personagemStore.fecharModal">&#x2715;</div>
   </dialog>
 </template>
