3757e3a7427df1c0c4fd6d1bfa999288dea8a7f1
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 11:51:25 -0300

    Correção do disabled para os dois botões de paginação

--- src/shared/components/Paginacao.vue
+++ src/shared/components/Paginacao.vue
@@ -16,7 +16,7 @@ const mostrarPaginacao = computed(() => {
   <div v-if="mostrarPaginacao" class="flex flex-col items-center justify-center gap-1 mt-4">
     <p class="text-main-text">Página {{personagemStore.filtros.page}} de {{ personagemStore.informacoesPaginacao?.pages }}</p>
     <div class="flex gap-4">
-      <button :class="['px-4 py-1 rounded-lg border transition-all text-alive border-alive text-shadow-glow shadow-[0_0_6px] shadow-alive hover:scale-105', personagemStore.paginaAnteriorDisponivel ? '' : 'opacity-50 cursor-not-allowed hover:scale-100']" @click="personagemStore.paginaAnterior"><span>⭠ </span>Página Anterior</button>
+      <button :class="['px-4 py-1 rounded-lg border transition-all text-alive border-alive text-shadow-glow shadow-[0_0_6px] shadow-alive hover:scale-105', personagemStore.paginaAnteriorDisponivel ? '' : 'opacity-50 cursor-not-allowed hover:scale-100']" :disabled="!personagemStore.paginaAnteriorDisponivel"  @click="personagemStore.paginaAnterior"><span>⭠ </span>Página Anterior</button>
       <button :class="['px-4 py-1 rounded-lg border transition-all text-alive border-alive text-shadow-glow shadow-[0_0_6px] shadow-alive hover:scale-105', personagemStore.proximaPaginaDisponivel ? '' : 'opacity-50 cursor-not-allowed hover:scale-100']" :disabled="!personagemStore.proximaPaginaDisponivel" @click="personagemStore.proximaPagina">Próxima página <span> ⭢</span></button>
     </div>
   </div>
