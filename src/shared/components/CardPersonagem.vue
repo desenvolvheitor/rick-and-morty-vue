bb8c481a20d3f9a6dab1166a09c11245e921f880
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 15:57:30 -0300

    Estilização de bordas

--- src/shared/components/CardPersonagem.vue
+++ src/shared/components/CardPersonagem.vue
@@ -14,7 +14,7 @@ const statusInfo = computed(() => {
 <template>
   <li :title="personagem.name" class="rounded-2xl overflow-hidden bg-primary flex flex-col w-full transition-all duration-200 ease-in-out group hover:cursor-pointer hover:border hover:border-alive hover:scale-105">
     <img :src="props.personagem.image" :alt="props.personagem.name" class="text-main-text">
-    <div class="py-2 px-3">
+    <div class="py-2 px-3 rounded-b-2xl border border-tertiary">
       <h3 class="font-orbitron truncate text-main-text text-lg group-hover:text-alive"><strong>{{ props.personagem.name }}</strong></h3>
       <p class="text-main-text">{{ props.personagem.species }}</p>
       <p :class="statusInfo.color">{{statusInfo.emoji}} {{ props.personagem.status }}</p>
