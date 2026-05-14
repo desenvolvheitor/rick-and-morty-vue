6aa6ba4c4b1e19e1728053336ad3e1dc8b777ac6
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 12:51:05 -0300

    Correção de respiro visual ao final do conteúdo de ListaPersonagens

--- src/shared/components/ListaPersonagens.vue
+++ src/shared/components/ListaPersonagens.vue
@@ -8,7 +8,7 @@ const personagemStore = usePersonagemStore();
 </script>
 
 <template>
-  <div class="flex flex-col items-center h-full">
+  <div class="flex flex-col items-center min-h-full pb-1">
     <Carregamento v-if="personagemStore.carregando" />
     <ul v-else class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
       <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem" @click="personagemStore.selecionarPersonagem(personagem), personagemStore.exibirModal" />
