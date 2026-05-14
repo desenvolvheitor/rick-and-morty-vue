a8d5ebc066ee2bf1515d9eaf460da2c96b7a977b
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-14 11:56:30 -0300

    Criação e uso do componente de carregamento

---
+++ src/shared/components/Carregamento.vue
@@ -0,0 +1,6 @@
+<template>
+ <div class="flex-1 flex flex-col items-center justify-center">
+    <img class="h-50 w-50 shadow-glow shadow-alive" src="../../assets/images/animacao-carregamento.webp">
+    <p class="mt-2 text-alive text-xl font-bold font-orbitron animate-pulse">Carregando...</p>
+  </div>
+</template>
--- src/shared/components/ListaPersonagens.vue
+++ src/shared/components/ListaPersonagens.vue
@@ -2,14 +2,19 @@
 import { usePersonagemStore } from '@/shared/stores/personagemStore';
 import CardPersonagem from './CardPersonagem.vue';
 import Paginacao from './Paginacao.vue';
+import Modal from './Modal.vue';
+import Carregando from './Carregamento.vue';
 const personagemStore = usePersonagemStore();
 </script>
 
 <template>
-  <ul class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-    <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem" @click="personagemStore.selecionarPersonagem(personagem)" />
-  </ul>
-  <Modal v-if="personagemStore.personagemSelecionado" :personagem="personagemStore.personagemSelecionado" />
-  <Paginacao />
+  <div class="flex flex-col justify-center items-center h-full">
+    <Carregando v-if="personagemStore.carregando" />
+    <ul v-else class="list-none grid gap-5 content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+      <CardPersonagem v-for="personagem in personagemStore.listaPersonagens" :key="personagem.id" :personagem="personagem" @click="personagemStore.selecionarPersonagem(personagem), personagemStore.exibirModal" />
+    </ul>
+    <Modal v-if="personagemStore.personagemSelecionado" :personagem="personagemStore.personagemSelecionado" />
+    <Paginacao />
+  </div>
 </template>
 
