2e9b51b01b10a41022a95466e2daaf6948c66834
Heitor Sales <desenvolvheitor@gmail.com>
2026-05-15 12:32:23 -0300

    Criação e estilização de backdrop para menu quando for mobile

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -16,6 +16,9 @@ onMounted(() => personagemStore.buscarPersonagens());
     <Header />
 
     <div class="flex-1 grid lg:grid-cols-[20%_80%] w-full overflow-hidden">
+      <Transition enter-active-class="transition-opacity duration-200 ease-out" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="transition-opacity duration-200 ease-in" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <div v-if="personagemStore.isMenuAberto" class="bg-black/30 fixed z-40 h-dvh w-dvw lg:hidden backdrop-blur-[3px]" @click="personagemStore.isMenuAberto = !personagemStore.isMenuAberto" ></div>
+      </Transition>
       <SideBar />
 
       <main class="bg-secondary p-[30px_40px] overflow-y-auto">
